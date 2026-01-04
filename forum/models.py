01e4603bd767a55904ee352ca89de9f219588665
Chitransh <chitransh1512@gmail.com>
2026-01-05 05:23:13 +0530

    Fix Category.save slug generation bug

--- forum/models.py
+++ forum/models.py
@@ -55,7 +55,7 @@ class Category(models.Model):
                 slug = f"{base_slug}-{counter}"
                 counter += 1
 
-        self.slug = slug
+            self.slug = slug
 
         super().save(*args, **kwargs)
 
